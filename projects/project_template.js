f7fd70a8e2830d974d9a6bc593305d1799300996
k1sh0re <109210914+k1sh0re@users.noreply.github.com>
2024-07-20 19:13:20 +0530

    added vtol project page

--- projects/project_template.js
+++ projects/project_template.js
@@ -1,42 +1,51 @@
 const LButton = document.getElementById('slideLeft');
 const RButton = document.getElementById('slideRight');
-var scroll_id = document.getElementById('project_image_scroll');
+const scroll_id = document.getElementById('project_image_scroll');
+const image_ids = document.querySelectorAll(".project_image");
+const num_images = parseInt(document.getElementById('num_images').textContent);
 const W = scroll_id.scrollWidth ;
-reset_colors(1+parseInt(scroll_id.scrollLeft/W*4));
+
+reset_colors(1+parseInt(scroll_id.scrollLeft/W*num_images));
 
 scroll_id.addEventListener("scroll", () => {
     const W = scroll_id.scrollWidth;
-    reset_colors(1 + parseInt(scroll_id.scrollLeft / W * 4));
+    reset_colors(1 + parseInt(scroll_id.scrollLeft / W * num_images));
 });
 
 LButton.onclick = () => {
-    const W = scroll_id.scrollWidth ;// document.documentElement.clientWidth
-
-    if (parseInt(scroll_id.scrollLeft/W*4) == 0){
-        scroll_id.scrollLeft += 3*W/4;
+    const W = scroll_id.scrollWidth ;
+    fade_toggle();
+    
+    if (parseInt(scroll_id.scrollLeft/W*num_images) == 0){
+        scroll_id.scrollLeft += (num_images-1)*W/num_images;
     }
     else{
-        scroll_id.scrollLeft -= W/4;
+        scroll_id.scrollLeft -= W/num_images;
     }
-    // scroll_id.scrollLeft -= W/4;
-    reset_colors(1+parseInt(scroll_id.scrollLeft/W*4));
-
+    reset_colors(1+parseInt(scroll_id.scrollLeft/W*num_images));
 }
 
 RButton.onclick = () => {
-    const W = scroll_id.scrollWidth; //document.documentElement.clientWidth
-    if (parseInt(scroll_id.scrollLeft/W*4) == 3){
-        scroll_id.scrollLeft -= 3*W/4;
+    const W = scroll_id.scrollWidth; 
+    fade_toggle();
+    if (parseInt(scroll_id.scrollLeft/W*num_images) == (num_images-1)){
+        scroll_id.scrollLeft -= (num_images-1)*W/num_images;
     }
     else{
-        scroll_id.scrollLeft += W/4;
+        scroll_id.scrollLeft += W/num_images;
     }
-    reset_colors(1+parseInt(scroll_id.scrollLeft/W*4));
+    reset_colors(1+parseInt(scroll_id.scrollLeft/W*num_images));
+    
 }
 
 function reset_colors(id){
-    for (let i=1; i<5; i++){
+    for (let i=1; i<num_images+1; i++){
         document.getElementById(i).style.color =  "#0087ca";
     }
     document.getElementById(id).style.color = "black";
+}
+
+function fade_toggle(){
+    image_ids.forEach((el) => {el.classList.add('fade');});
+    setTimeout(function() {image_ids.forEach((el) => {el.classList.remove('fade');});}, 400);
 }
